b9ffb73a706349b0493d7abbf2d388c2a635687c
keitheorem <152051386+keitheorem@users.noreply.github.com>
2024-09-24 22:20:18 +0800

    Add files via upload

---
+++ week_7_content/prime_numbers.py
@@ -0,0 +1,22 @@
+counter = 0
+
+# Get numbers 1 to 1000
+for i in range(2,1001):
+    is_prime = True
+
+    # Use this loop to determine if it is a prime number or not 
+    for a in range(2,i): 
+        if i%a == 0: 
+            is_prime = False
+            break
+
+    # If is_prime == True: 
+    if is_prime: 
+        print(f"{i:>4}", end = " ")
+        counter += 1
+    
+    # after every 10 prime numbers, print a space
+    if counter%10 == 0: 
+        print("")
+
+
